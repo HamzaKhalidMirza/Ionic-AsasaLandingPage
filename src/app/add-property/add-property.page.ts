b403a2589e81a7cfa085b1c6ead93742ed43b9bb
HamzaKhalidMirza <hamzakhalid.zx@gmail.com>
2020-09-08 15:49:00 +0500

    Validate AddPropertyForm-Ionic4

--- src/app/add-property/add-property.page.ts
+++ src/app/add-property/add-property.page.ts
@@ -18,33 +18,80 @@ export class AddPropertyPage implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.formInitializer();
     this.selection = 'Sale';
     this.areaUnits = [ 'Kanal', 'Marla', 'Sq. Feet', 'Sq. Yard' ];
     this.propertyTypes = [ 'Apartment', 'House','Penthouse', 'Farm House',
                            'Shop', 'Plaza', 'Office', 'Floor',
                            'Residential Plot', 'Commercial Plot', 'Farm House Plot' ];
-    this.formInitializer();
+
+    this.addPropertyForm.patchValue({sale_rent: this.selection});
   }
 
   formInitializer() {
     this.addPropertyForm = this.formBuilder.group({
-      name: [''],
-      email: [''],
-      mobileNo: [''],
-      city: [''],
-      propertyDetails: [''],
-      propertyType: [''],
-      landArea: [''],
-      landAreaUnit: [''],
-      demandArea: [''],
+      sale_rent: ['', [Validators.required]],
+      userName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      mobileNo: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      propertyDetails: ['', [Validators.required]],
+      propertyType: ['', [Validators.required]],
+      landArea: ['', [Validators.required]],
+      landAreaUnit: ['', [Validators.required]],
+      demand: ['', [Validators.required]],
       message: ['']
     });
   }
 
+  get SaleRent() {
+    return this.addPropertyForm.get("sale_rent");
+  }
+  get Username() {
+    return this.addPropertyForm.get("userName");
+  }
+  get Email() {
+    return this.addPropertyForm.get("email");
+  }
+  get MobileNo() {
+    return this.addPropertyForm.get("mobileNo");
+  }
+  get City() {
+    return this.addPropertyForm.get("city");
+  }
+  get PropertyDetails() {
+    return this.addPropertyForm.get("propertyDetails");
+  }
+  get PropertyType() {
+    return this.addPropertyForm.get("propertyType");
+  }
+  get LandArea() {
+    return this.addPropertyForm.get("landArea");
+  }
+  get LandAreaUnit() {
+    return this.addPropertyForm.get("landAreaUnit");
+  }
+  get Demand() {
+    return this.addPropertyForm.get("demand");
+  }
+  get Message() {
+    return this.addPropertyForm.get("message");
+  }
+
+  addProperty() {
+    console.log(this.addPropertyForm);
+    if(this.addPropertyForm.invalid) {
+      return;
+    }
+    console.log(this.addPropertyForm.value);
+  }
+
   selectSale() {
     this.selection = 'Sale';
+    this.addPropertyForm.patchValue({sale_rent: this.selection});
   }
   selectRent() {
     this.selection = 'Rent';
+    this.addPropertyForm.patchValue({sale_rent: this.selection});
   }
 }
